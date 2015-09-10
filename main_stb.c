e59c7260f7665b2574154848575a07e1e204b5bb
opglea <sarin@posteo.de>
2015-09-10 21:52:11 +0200

    Mysql Connection works

--- main_stb.c
+++ main_stb.c
@@ -1,13 +1,25 @@
-#include <stdio.h>
 #include <mysql/mysql.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+int main( void) {
+   MYSQL *conn;
+   
+   char *server = "192.168.178.43";
+   char *user = "monty";
+   char *password = "wert12345"; /* set me first */
+   char *database = "mysql";
+   conn = mysql_init(NULL);
+   /* Connect to database */
+   if (!mysql_real_connect(conn, server,
+         user, password, database, 0, NULL, 0)) {
+      fprintf(stderr, "%s\n", mysql_error(conn));
+      exit(1);
+   }
+   
 
-int main ()
-{
-	
-	//First Test with git
 	printf("Mysql client version: %s\n", mysql_get_client_info());
-	
-	
-return 0;
-}
+	        
+	        
+   return 0;
+  }
