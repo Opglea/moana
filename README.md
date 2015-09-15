c9c91e07dcea102065089ddc529a5821ef70d7bc
opglea <sarin@posteo.de>
2015-09-15 23:18:21 +0200

    Config File steht nun für Mysql Connection bereit(moana.conf)!

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 final project David &amp; Andi
 
 simple C/C++ Programm to get information from microcontroller, who is connected with a door lock. Write Data in mysql database and recive confirmation from database for granted entrance. + logging time of granted access.
-sdf
+
+
+moana.conf must be in the same folder 15092015
--- main_stb.c
+++ main_stb.c
@@ -1,21 +1,78 @@
 #include <mysql/mysql.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main( void) {
-   MYSQL *conn;
-   
-   char *server = "192.168.178.43";
-   char *user = "monty";
-   char *password = "wert12345"; /* set me first */
-   char *database = "mysql";
-   conn = mysql_init(NULL);
-   /* Connect to database */
-   if (!mysql_real_connect(conn, server,
-         user, password, database, 0, NULL, 0)) {
-      fprintf(stderr, "%s\n", mysql_error(conn));
-      exit(1);
-   }
+//#######################
+//## moana.conf als Mysql Config file wird benötigt im selben Ordner wie
+//## das Main Programm
+
+int main(void) {
+
+	
+	MYSQL *conn;
+	char configFilename[50]={"moana.conf"},inputLine[6][50];
+	FILE *configFile;
+	int c,i=0;
+	long int mysqlPort;
+	
+	//Datenbankconnection und splitten der Strings
+	char *pch;
+	char mysqlConfig[6][50];
+	char *pserver, *puser, *ppassword, *pdatabase, *pport;
+	char *errorStringtoPort;
+	
+	configFile = fopen("moana.conf","r");
+	
+	if(configFile != NULL){												//Wenn die File ok ist weiter
+		while((c=fgetc(configFile)) != EOF){							//Solange fortsetzen wie das Ende Der File erreicht ist
+			for(i=0;i<7;i++){											//Zeilenweisen einlesen der Config #Problem:Ersteszeichen 
+				fgets(inputLine[i],50,configFile);							
+					
+				if(inputLine[i][(int)strlen(inputLine[i])-1]==10)		//Löschen des CR(\n)
+					inputLine[i][(int)strlen(inputLine[i])-1]= 0 ;		
+			}
+		}
+	}else{
+		printf("could not open Config File # %s #",configFilename);
+		return EXIT_FAILURE;
+	}
+	
+		
+	pserver = mysqlConfig[0];
+	puser = mysqlConfig[1];
+	ppassword = mysqlConfig[2];
+	pdatabase = mysqlConfig[3];
+	pport = mysqlConfig[4];
+	
+	mysqlPort = strtol(mysqlConfig[4],&errorStringtoPort,10);
+	
+	//Die Config Strings trennen und neu speichern
+	for(i=0;i<5;i++){
+		pch = strtok(inputLine[i+1],"=");
+		pch=strtok(NULL, "=");
+		strncpy(mysqlConfig[i],pch,sizeof(mysqlConfig[i]));
+		}	
+		
+	/*for(i=0;i<6;i++)
+		puts(mysqlConfig[i]);
+	*/
+			
+	fclose(configFile);
+	
+	
+	//***************************************************
+	//****************MySQL CONNECTION*******************
+	//***************************************************
+	
+	conn = mysql_init(NULL);
+		
+	// Connect to database 
+	if(!mysql_real_connect(conn, pserver,
+			 puser, ppassword, pdatabase, mysqlPort, NULL, 0)) {
+		fprintf(stderr, "%s\n", mysql_error(conn));
+		exit(1);
+	   }
    
 
 	printf("Mysql client version: %s\n", mysql_get_client_info());
